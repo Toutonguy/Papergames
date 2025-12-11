c005514addf3c3147a25f8dbd9a3e0ffb6d5c66c
Toutonguy <ahtoutoungi@perse.co.uk>
2025-12-11 20:07:26 +0000

    Update online.js

--- scripts/online.js
+++ scripts/online.js
@@ -1,30 +1,21 @@
-<!-- /scripts/online.html -->
-<script>
-  function createRoom(gameName) {
-    const roomId = Math.random().toString(36).substring(2, 8);
-    const ref = db.ref(`games/${gameName}/rooms/${roomId}`);
-    ref.set({
-      createdAt: Date.now()
-    });
-    return roomId;
-  }
+function createRoom(gameName) {
+  const roomId = Math.random().toString(36).substring(2, 8);
+  const ref = db.ref(`games/${gameName}/rooms/${roomId}`);
+  ref.set({ createdAt: Date.now() });
+  return roomId;
+}
 
-  function joinRoom(gameName, roomId) {
-    return db.ref(`games/${gameName}/rooms/${roomId}`).get();
-  }
+function joinRoom(gameName, roomId) {
+  return db.ref(`games/${gameName}/rooms/${roomId}`).get();
+}
 
-  function sendMove(gameName, roomId, moveObj) {
-    const movesRef = db.ref(`games/${gameName}/rooms/${roomId}/moves`);
-    movesRef.push({
-      ...moveObj,
-      t: Date.now()
-    });
-  }
+function sendMove(gameName, roomId, moveObj) {
+  const movesRef = db.ref(`games/${gameName}/rooms/${roomId}/moves`);
+  movesRef.push({ ...moveObj, t: Date.now() });
+}
 
-  function onMove(gameName, roomId, callback) {
-    const movesRef = db.ref(`games/${gameName}/rooms/${roomId}/moves`);
-    movesRef.on("child_added", snap => {
-      callback(snap.val());
-    });
-  }
-</script>
+function onMove(gameName, roomId, callback) {
+  const movesRef = db.ref(`games/${gameName}/rooms/${roomId}/moves`);
+  movesRef.on("child_added", snap => callback(snap.val()));
+}
+  
